30c6716b02e8fa9c392f860fb481d7e374e8f43e
Glitched Polygons <glitchedpolygons@protonmail.com>
2022-03-18 12:02:47 +0100

    Added ByteArrayExtensions::ToBase64String() and upgraded to .NET 6.0

--- src/ByteArrayExtensions.cs
+++ src/ByteArrayExtensions.cs
@@ -46,5 +46,17 @@ namespace GlitchedPolygons.ExtensionMethods
         {
             return Encoding.UTF8.GetString(bytes);
         }
+        
+        /// <summary>
+        /// Converts a <c>byte[]</c> array to a Base64-encoded <see cref="String"/> with optional removal of the padding '=' characters.
+        /// </summary>
+        /// <param name="bytes">The bytes to encode.</param>
+        /// <param name="omitPaddingChars">Should the <c>=</c> padding characters be omitted?</param>
+        /// <returns>The Base64-encoded string.</returns>
+        public static string ToBase64String(this byte[] bytes, bool omitPaddingChars = false)
+        {
+            string output = Convert.ToBase64String(bytes);
+            return omitPaddingChars ? output.TrimEnd('=') : output;
+        }
     }
 }
